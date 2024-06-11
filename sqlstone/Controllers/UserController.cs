a003b283f42cc94abd5480db1ad1b1d462ae1e07
raddevus <1radeutsch1@gmail.com>
2024-06-11 12:28:13 -0400

    Continuing work to create registered user-folder & user-owned db.

--- sqlstone/Controllers/UserController.cs
+++ sqlstone/Controllers/UserController.cs
@@ -23,10 +23,21 @@ public class UserController : Controller
         Console.WriteLine($"uuid: {uuid}");
         User u = new User(uuid);
         var ipAddr = HelperTool.GetIpAddress(Request);
-        UuidInfo info = new UuidInfo{Uuid=uuid,IpAddr=ipAddr};
-        UuidInfoContext uuidCtx = new UuidInfoContext(contentRootPath);
-        uuidCtx.Add(info);
-        uuidCtx.SaveChanges();
+        
+        var userDir = Path.Combine(webRootPath,uuid);
+
+        if (!Directory.Exists(userDir)){
+            Directory.CreateDirectory(userDir);
+            if (!System.IO.File.Exists( Path.Combine(userDir,"sqlstoneu.db"))){
+                UuidInfo info = new UuidInfo{Uuid=uuid,IpAddr=ipAddr};
+                UuidInfoContext uuidCtx = new UuidInfoContext(contentRootPath);
+                uuidCtx.Add(info);
+                uuidCtx.SaveChanges();
+            }
+            else{
+                Console.WriteLine("User is already registered.");
+            }
+        }
 
         return new JsonResult(new {result=true, directory=webRootPath, ip=ipAddr});
     }
