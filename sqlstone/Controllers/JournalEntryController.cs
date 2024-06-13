61079809abde23c9fd942b25eaeab13cd03b4915
raddevus <1radeutsch1@gmail.com>
2024-06-13 12:25:41 -0400

    Working code using the JournalEntry class and EF to get the results from the db & return JSON to web page. on GetAll

--- sqlstone/Controllers/JournalEntryController.cs
+++ sqlstone/Controllers/JournalEntryController.cs
@@ -25,9 +25,23 @@ public class JournalEntryController : Controller
     public ActionResult GetAll([FromForm] string uuid){
         var userDir = Path.Combine(webRootPath,uuid);
         var userDbFile = Path.Combine(userDir,journalTemplateDbFile);
-        List<dynamic> allItems = new List<dynamic>();
-        allItems.Add(new {id=0,title="WebAPI one",note="first data from webapi!",created="2024-04-04",updated=""});
-        allItems.Add(new {id=1,title="WebAPI 2nd one",note="API - Anyone running a small-biz SaaS & making $$$?",created="2024-04-06",updated=""});
+        
+        JournalEntryContext jec = new JournalEntryContext(userDbFile);
+
+        var entries = jec.JEntry;
+        
+        List<JournalEntry> allItems = new List<JournalEntry>();
+
+        foreach (JournalEntry je in entries.AsParallel<JournalEntry>()){
+            Console.WriteLine($"{je.Id} : {je.Title} : {je.Note}");
+            allItems.Add(je);
+        }
+
+        
+
+        // allItems.Add(new {id=0,title="WebAPI one",note="first data from webapi!",created="2024-04-04",updated=""});
+        // allItems.Add(new {id=1,title="WebAPI 2nd one",note="API - Anyone running a small-biz SaaS & making $$$?",created="2024-04-06",updated=""});
+
         return new JsonResult(allItems);
     }
 
--- sqlstone/Models/JournalEntry.cs
+++ sqlstone/Models/JournalEntry.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
 public class JournalEntry{
+    public Int64 Id{get;set;}
+    public String Title{get;set;}
+    public String Note{get;set;}
+    public String Created{get;set;}
+    public String? Updated{get;set;}
     
 }
