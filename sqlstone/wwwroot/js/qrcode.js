f3e3d203da0607b220f28c6f96747d2430a1e3af
raddevus <1radeutsch1@gmail.com>
2024-06-23 10:59:24 -0400

    Added all source code (new qrcode.js), .css and html items to complete the gen qr code functionality for the UUID.

---
+++ sqlstone/wwwroot/js/qrcode.js
@@ -0,0 +1,36 @@
+let genQRCodeButton;
+let qrImage;
+let qrFormIsVisible = false;
+
+document.body.addEventListener("mouseup", body_mouseup);
+genQRCodeButton = document.querySelector("#genQRButton");
+genQRCodeButton.addEventListener("click", genQRCodeButton_Click);
+
+function genQRCodeButton_Click(){
+
+    let uuidInput = document.querySelector("#uuid");
+    if (uuidInput.value == ""){
+        uuidRegisterAlert("There is no UUID value to generate a QRCode for.");
+        return;
+    }
+    console.log(uuidInput.value);
+
+    let qrForm = document.querySelector("#qrForm");
+    
+    qrForm.style.display = "block";
+    console.log(qrForm);
+    
+    
+    fetch("https://newlibre.com/QRCodeGen/QREncoder/GetBase64QR?inText=" + uuidInput.value)
+        .then(response => response.text())
+        .then(data => qrImage.src="data:image/png;base64," + data)
+        .then(qrFormIsVisible = true);
+}
+
+function body_mouseup(){
+    if (qrFormIsVisible){
+        let qrForm = document.querySelector("#qrForm");
+        qrForm.style.display = "none";
+        qrFormIsVisible = false;
+    }
+}
